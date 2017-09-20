6b0a1e3394dcd7db2d1370069a7f7551cb7da5a8
Jose-Gar <31524688+Jose-Gar@users.noreply.github.com>
2017-09-19 17:17:55 -0700

    Building Classes
    Made simple classes for building and fort

--- Assets/Scripts/Buildings/Building.cs
+++ Assets/Scripts/Buildings/Building.cs
@@ -4,13 +4,47 @@ using UnityEngine;
 
 public class Building : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    private int hp;
+    private int def;
+    private int goldPerTurn;
+
+    // Constructor
+    public Building()
+    {
+        this.hp = 100;
+        this.def = 100;
+        this.goldPerTurn = 10;
+    }
+
+    public int GetHP()
+    {
+        return this.hp;
+    }
+
+    public int GetDef()
+    {
+        return this.hp;
+    }
+
+    public int GetGoldPerTurn()
+    {
+        return this.goldPerTurn;
+    }
+
+}
+
+public class Fort : Building{
+
+    private int bonusRange;
+
+    // Constructor
+    public Fort()
+    {   
+        this.bonusRange = 1;
+    }
+
+    public int GetBonusRange()
+    {
+        return this.bonusRange;
+    }
 }
