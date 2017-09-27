aa456f2d7c58f375296698d7111d63c1c917c602
Mark Philipp <Wolfsenshi@gmail.com>
2017-09-26 22:28:42 -0700

    Added some summaries and comments

--- Assets/Scripts/Units/Unit.cs
+++ Assets/Scripts/Units/Unit.cs
@@ -33,43 +33,71 @@ public class Unit : MonoBehaviour
 
 	protected void FindTraversableTiles()
 	{
+		///<summary>
+		/// Primitive pathfinding implementation.
+		/// Code will look left, right, up, down, diagonal for tiles it can move to.
+		/// </summary>
+		
 		for (int i = -1; i < 2; i++)
 		{
 			for (int j = -1; j < 2; j++)
 			{
+				// we use nested for loops bounded from -1 to 1, which allows us to look at all adjacent tiles to our current position.
+				// To do this, we call x + i, x + j. For instance, [4 + -1, 4 + -1] will check the tile [3, 3]. The loop is nested because
+				// the next iteration will do [4 + -1, 4 + 0], which checks tile [3, 4].
+
 				if (i == 0 && j == 0)
-					continue;
+					continue; // this continues loop if our [x + i, y + j] is equal to our current position. We don't want to check our pos.
+
+				// The below if statement performs bounds checking to make sure our next tile is within the bounds of our 2D tile array.
+				// tiles.GetLength(0) grabs the 0th dimension of the tiles array and returns it's length.
+				// For instance, if tiles is size [3][4]. tiles.GetLength(0) will return 3. GetLength(1) will return 4
 
 				if (x + i > GridManager.gridMan.tiles.GetLength(0) - 1 || x + i < 0 || y + j > GridManager.gridMan.tiles.GetLength(1) - 1 || y + j < 0)
 					continue;
 
-				Tile currentTile = GridManager.gridMan.tiles[x + i, y + j];
+				Tile currentTile = GridManager.gridMan.tiles[x + i, y + j]; // Set our current tile. We don't start with our unit's pos.
 
-				float totalCost = 0;
+				float totalCost = 0; // initialize our totalCost variable. This will keep a running tally of the movement cost in a direction.
 
 				while (totalCost <= movement)
 				{
+					// This loop will run as long as total cost does not exceed our unit's movement.
+
+					// the below equation adds the distance from our currentTile to our previous tile and adds the movementModifier of the current tile. The weird division black magic is necessary due to the way the GridLayoutManager is implemented. In most circumstances, you would just run Vector2.Distance between one position and another position.
 					totalCost += (Vector2.Distance(currentTile.gridPosition, GridManager.gridMan.tiles[currentTile.x - i, currentTile.y - j].gridPosition) / GridManager.gridMan.grid.cellSize.x) + currentTile.movementModifier;
 
 					if (totalCost <= movement && currentTile.unit == null)
-						currentTile.SetTraversable();
+						// If our cost has not exceeded movement and no unit is on the current tile, then we can move to this tile.
+						currentTile.SetTraversable(); 
 					else if (totalCost > movement)
+						// If our totalCost has exceeded our movement, then we can't access the currentTile. Break loop and start next search
 						break;
 
-					// bounds checking
+					// bounds checking again
 					if (currentTile.x + i > GridManager.gridMan.tiles.GetLength(0) - 1 || currentTile.x + i < 0 || currentTile.y + j > GridManager.gridMan.tiles.GetLength(1) - 1 || currentTile.y + j < 0)
 						break;
 
 					currentTile = GridManager.gridMan.tiles[currentTile.x + i, currentTile.y + j]; // assign next tile
 				}
 			}
-
 		}
 	}
 	
 	private void OnMouseDown()
 	{
-
+		///<summary>
+		/// This function is called whenever a unit is clicked on with the mouse. We need this function to have several if statements
+		/// 
+		/// 1. If Statement 1 will set our selected unit and find traversible tiles for movement.
+		///		- If this unit is not selected AND has not attacked AND the Unit Manager's selectedUnit is null, THEN set this unit toselected, set the Unit Manager's selectedUnit to this, and FindTraversibleTiles()
+		///	
+		/// 2. else if statement 1 will commence the AttackRoutine() function.
+		///		- If this unit is not selected AND Unit MAnager's selectedUnit is NOT this, AND Unit Manager's selectedUnit is NOT null, THEN call AttackRoutine()
+		///	
+		/// 3. else if statement 2 will cancel unit selection if we click on the selected unit again.
+		///		- If this unit is selected, THEN set this unit to not selected, set Unit Manager's selectedUnit to null
+		/// </summary>
 		if (!isSelected && !hasAttacked && UnitManager.unitManager.selectedUnit == null)
 		{
 			// if no unit is selected at all, select this unit for movement/attacking.
@@ -81,7 +109,7 @@ public class Unit : MonoBehaviour
 		else if(!isSelected && UnitManager.unitManager.selectedUnit != this && UnitManager.unitManager.selectedUnit != null /* && UnitManager.selectedUnit.player != this.player*/)
 		{
 			// This means a different unit is selected and they are attempting to attack this unit
-			AttackRoutine();
+			AttackRoutine(UnitManager.unitManager.selectedUnit);
 		}
 		else if (isSelected)
 		{
@@ -115,52 +143,48 @@ public class Unit : MonoBehaviour
 
 	void HasAttacked()
 	{
+		///<summary>
+		/// This function will do three things:
+		/// 1. Set isSelected to false
+		/// 2. set hasAttacked to true
+		/// 3. set the Unit Manager's selectedUnit to null
+		/// 
+		/// This function will be called at the end of the attack routine. It will be called on the attacking unit.
+		/// E.G:    attackingUnit.HasAttacked();
+		/// </summary>
 		isSelected = false;
 		hasAttacked = true;
 		UnitManager.unitManager.selectedUnit = null;
 	}
 
-	void AttackRoutine()
+	void AttackRoutine(Unit attackingUnit)
 	{
-		// check if attacker is in range of our potential defending unit
-		if (!CheckAttackRange())
+		// check if attacker is in range of this unit
+		if (!attackingUnit.CheckAttackRange(this))
 			return; // exit the function if not in range
 
 		// If we make it here, then we're in range. Execute unit attack against this unit
-		UnitManager.unitManager.selectedUnit.Attack(this); // attack unit and make it take damage
+		attackingUnit.Attack(this); // attack unit and make it take damage
 
 		// now the unit gets to return fire if they are in range
-		if (!CheckReturnFireRange())
+		if (!this.CheckAttackRange(attackingUnit))
 			return;
 
 		// if we make it here, then defender is in range. Execute a counter attack against attacking unit
-		this.Attack(UnitManager.unitManager.selectedUnit);
+		this.Attack(attackingUnit);
 
 		// if we make it all the way here, then we can set the selectedUnit's turn to be over
-		UnitManager.unitManager.selectedUnit.HasAttacked();
+		attackingUnit.HasAttacked();
 	}
 
-	bool CheckAttackRange()
+	bool CheckAttackRange(Unit defendingUnit)
 	{
 		/// <summary>
 		/// Check if enemy unit is in range to attack. Returns true if distance is within range. False otherwise.
 		/// Terrain modifiers for movement are not considered. This is a standard distance calculation with no weights.
 		/// </summary>
 		
-		if (Vector2.Distance(gridPosition, UnitManager.unitManager.selectedUnit.gridPosition) > UnitManager.unitManager.selectedUnit.range)
-			return false;
-		else
-			return true;
-	}
-
-	bool CheckReturnFireRange()
-	{
-		/// <summary>
-		/// Check if the defending unit is in attack range of the attacking unit.
-		/// As with the CheckAttackRange function, terrain movement modifiers are not considered here.
-		/// </summary>
-
-		if (Vector2.Distance(gridPosition, UnitManager.unitManager.selectedUnit.gridPosition) > this.range)
+		if (Vector2.Distance(gridPosition, defendingUnit.gridPosition) > this.range)
 			return false;
 		else
 			return true;
@@ -180,8 +204,26 @@ public class Unit : MonoBehaviour
 	void TakeDamage(int attackDamage, int attackerHP)
 	{
 		///<summary>
-		/// Perform damage calculations against this unit.
-		/// attackDamage variable includes terrain modifiers since Tile class is handling updating those stats.
+		/// Perform damage calculations against the unit that calls this function.
+		/// 
+		/// This function should implement our equations we came up with
+		/// 
+		/// NOTE: attackDamage variable includes terrain modifiers since Tile class is handling updating those stats. Yo udo not have to worry about handling those modifiers in this function
+		/// 
+		/// ANOTHER NOTE: Casting gets tricky in this function. We only want to remove the decimal place when we go to subtract totalDamageDone from hp. the totalDamageDone variable needs to have decimal precision. However, keep in mind that all our variables we're working with are integers. 
+		/// 
+		/// My hint would be that you can create float values in C# by putting an 'f' at the end of a number. 1f, 3f, 2.5f, etc. Remember, if you have integers performing any math with a floating number, the result will be cast to a float.
+		/// 
+		/// Additionally, you can cast values in C# by putting (type) in front of the variable you want to cast. For instance
+		/// 
+		/// float x = (float) 3; // this will turn x into 3.0
+		/// 
+		/// 1.	totalDamageDone = equation we came up with.
+		/// 2.	If statement 1: if totalDamageDone less than 0.5, THEN totalDamageDone = 0
+		/// 3.	if statement 2: if totalDamageDone greater or equal to 0.5 AND above result less than 1, THEN totalDamageDone = 1
+		/// 4.	subtract totalDamageDone from defending unit's hp
+		/// 5.	For testing, I recommend putting a Debug.Log(transform.name + " Took " + finalDamage + " Damage") statement at end of function.
+		/// 
 		/// </summary>
 
 		float totalDamage = attackDamage * ((attackerHP / 10f) * (attackDamage / this.defense));
