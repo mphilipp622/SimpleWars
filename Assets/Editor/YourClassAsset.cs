c4dad2dda6ed4acf503ac37be8e91657d72f4648
sche09 <31779311+sche09@users.noreply.github.com>
2017-09-28 16:22:09 -0700

    Unit Script Update
    - Generic Attributes added
    - Need to factor in (x,y) elements

---
+++ Assets/Editor/YourClassAsset.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public class YourClassAsset
+{
+	[MenuItem("Isaac Tools/Create New Tile")]
+	public static void CreateTile()
+	{
+		TilePopupWindow.Init();
+	}
+}
---
+++ Assets/Scripts/Units/UnitManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnitManager : MonoBehaviour
+{
+	public static UnitManager unitManager;
+
+	Unit _selectedUnit;
+
+	public Unit selectedUnit
+	{
+		get
+		{
+			return _selectedUnit;
+		}
+		set
+		{
+			_selectedUnit = value;
+		}
+	}
+
+	private void Awake()
+	{
+		InitSingleton();
+	}
+
+	void Start()
+	{
+
+	}
+
+	void Update()
+	{
+
+	}
+
+	void InitSingleton()
+	{
+		// singleton pattern. Assigns this instance of the class to gridMan if gridMan is null.
+		// destroys this instance if a gridMan already exists. We only want one of these in our game.
+		if (unitManager == null)
+			unitManager = this;
+		else if (unitManager != this)
+			Destroy(gameObject);
+	}
+}
