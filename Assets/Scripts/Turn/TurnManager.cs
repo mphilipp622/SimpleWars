4de5a01b13aa178e4d2275b6d3437e634245fc1b
J-Sesate <31783774+J-Sesate@users.noreply.github.com>
2017-10-07 10:56:11 -0700

    Turn Manager
    New code added for Turn Manager class.

--- Assets/Scripts/Turn/TurnManager.cs
+++ Assets/Scripts/Turn/TurnManager.cs
@@ -16,18 +16,31 @@ public class TurnManager : MonoBehaviour
     Player [] player = new Player [2]; //Creates array for the two store two Player class objects
     Player currentPlayer;  //Variable to represent the current player
     currentPlayer = player[0]; //Assigns the current Player to the first Player in player[]
+    int index = 0;
     
+    public static TurnManager turnManager;
+
     /*
         EndTurn() allows the current player to end their turn, locking them in from further input during
         the next Player's turn, assigns the currentPlayer to the next Player in player[], and increments the turn counter by 1.
     */
     
-    EndTurn()
+    public void EndTurn()
     {
-        currentPlayer.Lock();
-        players[index++ %= 2];
-        currentPlayer.startTurn();
-        turnCounter++;
+        currentPlayer.Lock(); //Locks the current Player object
+        index++;
+        currentPlayer = player[index %= 2]; //Assigns currentPlayer to the next Player object in player[]
+        currentPlayer.startTurn(); //Calls on the Player's startTurn function
+
+        turnCounter++; //Increments turn counter by 1
+    }
+
+    void initSingleton()
+    {
+        if(turnManager == null)
+            turnManager = this;
+        else if (turnManager != this)
+            Destroy(gameObject);
     }
 
     void start()
