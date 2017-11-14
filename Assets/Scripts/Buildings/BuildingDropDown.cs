89e81c1b44597a35500022c00cf7285b980c2f26
sche09 <31779311+sche09@users.noreply.github.com>
2017-11-14 14:41:28 -0800

    Updated DropDown Menu
    - Added script for testing
    - Created Prefab of dropdown menu with basic concepts

--- Assets/Scripts/Buildings/BuildingDropDown.cs
+++ Assets/Scripts/Buildings/BuildingDropDown.cs
@@ -1,16 +1,38 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class BuildingDropDown : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+public class BuildingDropDown : MonoBehaviour
+{
+    [Tooltip("The drop down box to populate with Unit Types")]
+    public Dropdown dropDown;
+
+    [Tooltip("Createable Units")]
+    public Sprite[] units;
+
+    void Start()
+    {
+        
+        dropDown.ClearOptions();
+
+
+        List<Dropdown.OptionData> unitTypes = new List<Dropdown.OptionData>();
+        foreach (var unitType in units)
+        {
+            string unitName = unitType.name;
+            int dot = unitType.name.IndexOf('.');
+            if (dot >= 0)
+            {
+                unitName = unitName.Substring(dot + 1);
+            }
+
+            // Add the option to the list
+            var unitOption = new Dropdown.OptionData(unitName, unitType);
+            unitTypes.Add(unitOption);
+        }
+
+        // Add the options to the drop down box
+        dropDown.AddOptions(unitTypes);
+    }
 }
