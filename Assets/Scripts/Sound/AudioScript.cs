721c2b2481ab187d7a73ca144a0d109658e5fbe7
Jose-Gar <josegarcia10101@gmail.com>
2017-11-14 20:36:20 -0800

    loops
    +loop dubstep
    +gunfire sfx

--- Assets/Scripts/Sound/AudioScript.cs
+++ Assets/Scripts/Sound/AudioScript.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class AudioScript : MonoBehaviour {
-    public AudioClip Loop1, Loop2;
+    public AudioClip Loop1, Loop2, Loop3, Loop4, Loop5;
     public DoubleAudioSource das;
     private void Awake()
     {
@@ -20,13 +20,25 @@ public class AudioScript : MonoBehaviour {
     
     public void SwitchMusic(int turn)
     {
-        if (turn == 5)
+        if (turn == 10)
         {
             das.CrossFade(Loop1, 1, 10, 0);
         }
-        else if (turn == 10)
+        else if (turn == 20)
         {
             das.CrossFade(Loop2, 1, 10, 0);
+        }
+        else if (turn == 60)
+        {
+            das.CrossFade(Loop3, 1, 10, 0);
+        }
+        else if (turn == 90)
+        {
+            das.CrossFade(Loop4, 1, 10, 0);
+        }
+        else if (turn == 120)
+        {
+            das.CrossFade(Loop5, 1, 10, 0);
         }
     }
 }
